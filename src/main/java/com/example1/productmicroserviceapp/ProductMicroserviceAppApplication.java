0d9f7a84aaf63591f0d588e32b084e4696a8cddb
kemalabdujebar <85251727+kemalabdujebar@users.noreply.github.com>
2024-05-11 20:58:47 +0300

    first commit

---
+++ src/main/java/com/example1/productmicroserviceapp/ProductMicroserviceAppApplication.java
@@ -0,0 +1,13 @@
+package com.example1.productmicroserviceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductMicroserviceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductMicroserviceAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example1/productmicroserviceapp/ProductMicroserviceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example1.productmicroserviceapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductMicroserviceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
